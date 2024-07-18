c26d7f7727c9d1c35021df14f48dca03faf1aed4
luigi moretti <luigi.moretti@ext.soprasteria.com>
2024-07-18 14:31:20 +0200

    bozza sezione foto

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,40 @@
-let currentSlide = 0;
+let currentSlide = { camera1: 0, camera2: 0, salone: 0 };
 
-function showSlide(index) {
-  const slides = document.querySelectorAll(".carousel-item");
+function showSlide(index, camera) {
+  const slides = document.querySelectorAll("." + camera + " .galleria-item");
+  const indicators = document.querySelectorAll("." + camera + " .indicator");
+
   if (index >= slides.length) {
-    currentSlide = 0;
+    currentSlide[camera] = 0;
   } else if (index < 0) {
-    currentSlide = slides.length - 1;
+    currentSlide[camera] = slides.length - 1;
   } else {
-    currentSlide = index;
+    currentSlide[camera] = index;
   }
-  const offset = -currentSlide * 100;
+  const offset = -currentSlide[camera] * 100;
   document.querySelector(
-    ".carousel-inner"
+    "." + camera + " .galleria-inner"
   ).style.transform = `translateX(${offset}%)`;
+
+  indicators.forEach((indicator, i) => {
+    if (i === currentSlide[camera]) {
+      indicator.classList.add("active");
+    } else {
+      indicator.classList.remove("active");
+    }
+  });
 }
 
-function nextSlide() {
-  showSlide(currentSlide + 1);
+function nextSlide(camera) {
+  showSlide(currentSlide[camera] + 1, camera);
 }
 
-function prevSlide() {
-  showSlide(currentSlide - 1);
+function prevSlide(camera) {
+  showSlide(currentSlide[camera] - 1, camera);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  showSlide(currentSlide);
+  for (let camera in currentSlide) {
+    showSlide(currentSlide[camera], camera);
+  }
 });
